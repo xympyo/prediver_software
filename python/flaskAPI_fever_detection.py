84d2fd329a4d016d80bcecffc2a678b688a8b123
Moshe Dayan <xynpyro@gmail.com>
2024-12-12 21:02:14 +0700

    updated python folder

--- python/flaskAPI_fever_detection.py
+++ python/flaskAPI_fever_detection.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.impute import SimpleImputer
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
